9a3a663ae2a50b7c5bae2e26dcd60e2f609803f4
yinxintao <yinxintao@twotiger.com>
2018-07-27 19:13:38 +0800

    html

--- www/handlers.py
+++ www/handlers.py
@@ -5,10 +5,13 @@ __author__ = 'Michael Liao'
 
 ' url handlers '
 
-import re,time
+import re,time,json,hashlib,base64,asyncio
+from www.apis import APIValueError,APIResourceNotFoundError,APIError
 from www.models import User,Blog,Comment,next_id
 from www.coreweb import get,post
 
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
 
 @get('/')
 async def index(request):
@@ -22,3 +25,33 @@ async def index(request):
         '__template__': 'blogs.html',
         'blogs': blogs
     }
+
+
+@get('/register')
+def register():
+    return {
+        '__template__': 'register.html',
+    }
+
+
+@post('/api/users')
+async def api_register_user(*,email,name,passwd):
+    if not name or not name.strip():
+        raise APIValueError('name')
+    if not email or not _RE_EMAIL.match(email):
+        raise APIValueError('email')
+    if not passwd or not _RE_SHA1.match(passwd):
+        raise APIValueError('passwd')
+
+    users = await User.findAll('eamail=?',[email])
+    if len(users) > 0:
+        raise APIError('register:failed', 'email', 'Email is already in use.')
+
+    uid = next_id()
+    sha1_passwd = 's%:s%' % (uid,passwd)
+    user = User(id= uid,name = name.strip(),email = email,passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),image='')
+
+
+
+
+
